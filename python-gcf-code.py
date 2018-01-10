f32abbec90a1293c695ae04bd0e010c53482a217
metacman <me_tacman@hotmail.com>
2018-01-10 11:21:49 -0600

    adding recently built python code

---
+++ python-gcf-code.py
@@ -0,0 +1,14 @@
+# compute GCF or GCD of 2 numbers
+# useful for reducing fractions
+
+def gcf (num1, num2):
+	if num1 > num2:
+		num1, num2 - num2, num1
+
+for x in range (num1, o, -1):
+	if num1 % x -- 0 and num2 % x -- 0:
+		return x
+
+num1 - 18
+num2 - 204
+print (str(gcf(num1, num2)))
